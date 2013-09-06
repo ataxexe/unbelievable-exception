377198400fdd3d470b3710ccc86fcf90f8d1319e
ataxexe <ataxexe@gmail.com>
2013-09-06 19:20:39 -0300

    mudança na estrutura do blog para melhorar o acesso às categorias por url

--- _plugins/categories.rb
+++ _plugins/categories.rb
@@ -4,15 +4,9 @@ module Jekyll
 
   module CategoryFilter
 
-    CATEGORY_DIR = 'categories'
-
     # build the url for a given category
     def category_url(category)
-      base_dir = @context.registers[:site].config['category_dir']
-      base_dir = (base_dir || CATEGORY_DIR).gsub(/^\/*(.*)\/*$/, '\1')
-      base_dir = I18n.transliterate(base_dir)
-      category = I18n.transliterate(category).gsub(/\s/, '-').downcase
-      File.join(base_dir, category)
+      I18n.transliterate(category).gsub(/\s/, '-').downcase
     end
 
   end
