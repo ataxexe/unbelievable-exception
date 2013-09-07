91fc58b54de37d812db9787cf670c2d96f54efa8
ataxexe <ataxexe@gmail.com>
2013-09-07 12:50:00 -0300

    automação da geração das páginas de categorias

--- _plugins/categories.rb
+++ _plugins/categories.rb
@@ -2,6 +2,41 @@ require "i18n"
 
 module Jekyll
 
+  class CategoryPage < Page
+
+    def initialize(site, base, category)
+      @site = site
+      @base = base
+      @dir = I18n.transliterate(category).gsub(/\s/, '-').downcase
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
+      self.data['index'] = category
+      @site.config['articles'].each do |name, categories|
+        categories.each do |cat|
+          if cat.downcase == category
+            self.data['title'] = cat
+            break
+          end
+        end
+      end
+    end
+  end
+
+  class CategoryPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      if site.layouts.key? 'category_index'
+        p site.categories
+        site.categories.keys.each do |category|
+          site.pages << CategoryPage.new(site, site.source, category)
+        end
+      end
+    end
+  end
+
   module CategoryFilter
 
     # build the url for a given category
