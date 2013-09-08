d57cbd1d8f5c8a13db757a094fffb40f61a63808
ataxexe <ataxexe@gmail.com>
2013-09-08 10:16:30 -0300

    não utilizado

--- _plugins/count.rb
+++ _plugins/count.rb
@@ -15,19 +15,6 @@ module Jekyll
     end
   end
 
-  class CategoryCount < Liquid::Tag
-
-    def initialize(tag_name, text, tokens)
-      super
-      @category = text
-    end
-
-    def render(context)
-      context.registers[:site].categories.size
-    end
-  end
-
 end
 
 Liquid::Template.register_tag('post_count', Jekyll::PostCount)
-Liquid::Template.register_tag('category_count', Jekyll::CategoryCount)
