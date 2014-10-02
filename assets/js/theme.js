e878e0596a2149d2fd1d017e1c945f7e9d0ce935
Marcelo Guimarães <ataxexe@gmail.com>
2014-10-02 14:58:37 -0300

    melhorias na troca de temas (agora nao vai mais dar merda)

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,14 +1,22 @@
-set_theme = function(theme) {
-  if (theme == undefined) {
-    theme = "dark"
-  }
-  localStorage.setItem("theme", theme)
-  var css = document.createElement('link')
-  css.setAttribute('rel', 'stylesheet')
-  css.setAttribute('src','http://example.com/site.js');
-  css.setAttribute('href', '/assets/css/bootstrap-' + theme + '.css')
-  css.setAttribute('type', 'text/css')
-  document.head.appendChild(css)
+THEMES = {
+  'dark' : '/assets/css/bootstrap-dark.css',
+  'light' : '/assets/css/bootstrap-light.css'
 }
 
-set_theme(localStorage.getItem("theme"))
+set_theme = function(theme) {
+  cssFile = THEMES[theme]
+  if(cssFile) {
+    localStorage.setItem("theme", theme)
+    var css = document.createElement('link')
+    css.setAttribute('rel', 'stylesheet')
+    css.setAttribute('href', cssFile)
+    css.setAttribute('type', 'text/css')
+    document.head.appendChild(css)
+    return true
+  }
+  return false
+}
+
+if(!set_theme(localStorage.getItem("theme"))) {
+  set_theme('dark')
+}
